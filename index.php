31a04f67f95ccc6392405d60f1a372257e628fa8
LevineRoss <63254933+LevineRoss@users.noreply.github.com>
2021-01-16 00:27:08 +0000

    core

---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+
+<html>
+<head>
+  <meta charset = "utf-8">
+  <meta name="viewport" content="width=device-width, inital-scale=1">
+
+  <title>Work Checklist</title>
+  <meta name = "description" content="21, From East London - United Kingdom.">
+  <meta name = "author" content="Ross Levine">
+  <link rel="shortcut icon" href="/img/favicon/favicon.68c7f017cfba.ico" type="image/x-icon">
+  <link rel="apple-touch-icon" href="img/favicon/apple-touch-icon-180x180.15a5044def06.png">
+  <link rel="mask-icon" href="img/favicon/favicon.25bc75538faa.svg" color="#005eb8">
+  <link rel="icon" sizes="192x192" href="img/favicon/favicon-192x192.43924bfe6c7e.png">
+  
+
+
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  <link rel = "stylesheet" href="css/global.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://kit-pro.fontawesome.com/releases/latest/css/pro.min.css" media="all">
+  <link rel="stylesheet" href="https://kit-pro.fontawesome.com/releases/latest/css/pro-v4-font-face.min.css" media="all">
+  <link rel="stylesheet" href="https://kit-pro.fontawesome.com/releases/latest/css/pro-v4-shims.min.css" media="all">
+</head>
+<body>
+
+<div class="container">
+    <div class="header">
+        WORK CHECKLIST
+    </div>
+    <div class="under-header">
+        Sophie is the best  
+    </div>
+
+</div>
+</div>
+</body>
+</html>
